d22051d6476e01f01be4ee14fa3fc86d788dc3db
shenzp <shenzp@qq.com>
2020-05-31 17:10:17 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/java/Customer.java
@@ -0,0 +1,8 @@
+package com.atguigu.java;
+
+public class Customer {
+
+    public static void main(String[] args){
+        System.out.println ("hello world!!!");
+    }
+}
---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.atguigu.java;
+
+public class HelloWorld {
+
+    public HelloWorld() {
+    }
+
+    public static void main(String[] args){
+        System.out.println ("hello world!");
+        System.out.println ("hello world!");
+
+    }
+
+}
